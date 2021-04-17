31343599dbf1dd45f4d8edbbbe028ea7bbb91fc7
PolozovaAnastasia1107 <81014539+PolozovaAnastasia1107@users.noreply.github.com>
2021-04-17 16:09:58 +0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,16 @@
+btn.addEventListener (click, Max(a,b))
+
+function Max()
+{
+    var a = document.getElementById("a")
+    var b = document.getElementById("b")
+    console.log(a)
+a-Number.parseInt(a)
+b-Number.parseInt(b)
+if (a>b)
+{
+    alert("Максимальное", a)
+}
+else
+alert("Максимальное", b);
+}
